757fcc587524a2896e481df96920a1d0acf47997
yasmimpamela <yasmim.eletraenergy@gmail.com>
2024-01-29 09:58:25 -0300

    Exercícios

---
+++ Yasmim/Aula3/src/main/java/app/aula3/ManipuladorNumeros.java
@@ -0,0 +1,6 @@
+package app.aula3;
+public class ManipuladorNumeros {
+    public int elevarAoQuadrado(int numero){
+        return numero * numero;
+    }
+}
---
+++ Yasmim/Aula4/src/Pai.java
@@ -0,0 +1,12 @@
+public class Pai extends Avo {
+
+    public Pai(String nome, String sobrenome, String cpf) {
+        super(nome, sobrenome, cpf);
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Nome: " + getNome());
+    }
+}
