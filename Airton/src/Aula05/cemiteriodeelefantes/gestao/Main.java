a65acc6827d774c11dd027664f870fd7e85221ff
AirtonSR <joseairton0976@gmail.com>
2024-02-05 10:26:24 -0300

    Aula05

---
+++ Airton/src/Aula05/cemiteriodeelefantes/gestao/Main.java
@@ -0,0 +1,14 @@
+package org.example.lesson_05.cemiteriodeelefantes.gestao;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Empregado empregado = new Empregado("Airton", "938321", 3000, "TI");
+        Gerente gerente = new Gerente("Airton", "938321", 3000, "Gerente");
+        gerente.adicionarSubordinado(empregado);
+        List<Empregado> listaDeEmpregados = gerente.getEmpregado();
+        System.out.println(listaDeEmpregados.toString());
+        empregado.calcularBonus(3000);
+        System.out.println(gerente.getEmpregado());
+    }
+}
