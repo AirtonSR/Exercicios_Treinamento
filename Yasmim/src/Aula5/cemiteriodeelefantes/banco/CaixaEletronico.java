40e28a4ff43d9c66adb95c904eb8e3503c5658a0
yasmimpamela <yasmim.eletraenergy@gmail.com>
2024-02-15 10:13:35 -0300

    delete

--- Yasmim/src/Aula5/cemiteriodeelefantes/banco/CaixaEletronico.java
+++ Yasmim/src/Aula5/cemiteriodeelefantes/banco/CaixaEletronico.java
@@ -3,10 +3,12 @@ package Aula5.cemiteriodeelefantes.banco;
 public class CaixaEletronico{
 
     public void sacar(ContaBancaria conta, double valor) {
+
         conta.sacar(valor);
     }
 
     public void depositar(ContaBancaria conta, double valor) {
+
         conta.depositar(valor);
         }
 
--- Yasmim/src/Desafio/Cargo.java
+++
@@ -1,7 +0,0 @@
-package Desafio;
-
-public enum Cargo {
-    DEV,
-    DBA,
-    TESTER;
-}
